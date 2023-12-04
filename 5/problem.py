51d2c524a1cefc2014461c4ab2969a785b0515a6
= <jeremy.turinetti@gmail.com>
2023-12-04 13:59:02 -0600

    prep for day 5

---
+++ 5/problem.py
@@ -0,0 +1,15 @@
+from utils import *
+from my_utils import read_aoc_data
+
+# solution functions
+def part_a(input):
+    # TODO
+    return
+
+def part_b(input):
+    # TODO
+    return
+
+input_data = read_aoc_data(X, Y)    # replace with correct day/year
+print(part_a(input_data))
+print(part_b(input_data))
