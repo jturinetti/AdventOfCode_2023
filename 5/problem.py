54dbfe4eac0bc1241620212db93229f1e7674b5a
= <jeremy.turinetti@gmail.com>
2023-12-05 08:41:10 -0600

    day 5 solutions (needs cleanup and optimization)

--- 5/problem.py
+++ 5/problem.py
@@ -2,16 +2,133 @@ import time
 from utils import *
 from my_utils import read_aoc_data
 
+def calculate_target_mapping(source_number, mapping_lists):
+    for index in range(len(mapping_lists)):
+        l = mapping_lists[index]
+        target_range_start = l[0]
+        source_range_start = l[1]
+        range_length = l[2]
+        if source_number >= source_range_start and source_number <= source_range_start + range_length - 1:
+            # print("YES")
+            # print("{} -> {}".format(source_number, l))
+            return (source_number - source_range_start + target_range_start)
+    
+    # print("NO, returning {}".format(source_number))
+    return source_number
+
+def construct_seed_array(input, part):
+    seed_array = lmap(lambda x: int(x), input[0].split(' ')[1::])
+    if (part == 'a'):
+        return seed_array
+    
+    print(seed_array)
+    if (part == 'b'):
+        expanded_seed_array = []
+        for index in range(0, len(seed_array), 2):
+            print(index)
+            temp = range(seed_array[index], seed_array[index] + seed_array[index + 1], 1)
+            print(temp)
+            expanded_seed_array.extend(range(seed_array[index], seed_array[index] + seed_array[index + 1], 1))
+        # print(expanded_seed_array)
+        return expanded_seed_array
+
+def solution_impl(input, seed_array):
+    mapping_dictionary = {}
+    name_dictionary = {}
+    # index into dictionary based on source string
+    # determine mapping value
+    # find next dictionary based on target string
+    current_target = ''
+    print(input)
+    for index in range(2, len(input)):
+        s = input[index]
+        # print(s)
+        if s == '':
+            continue
+        parsed_entry = s.split(' ')[0]
+        if not parsed_entry.isdigit():
+            parsed_mapping_names = parsed_entry.split('-')
+            # print(parsed_mapping_names)
+            source = parsed_mapping_names[0]
+            target = parsed_mapping_names[2]
+            name_dictionary[source] = target
+            current_target = target
+            mapping_dictionary[target] = []
+        else:
+            parsed_mapping_values = lmap(lambda x: int(x), s.split(' '))
+            mapping_dictionary[current_target].append(parsed_mapping_values)
+
+    # print(name_dictionary)
+    # print(mapping_dictionary)
+
+    locations = []
+    current_key = 'seed'
+    
+    # print(seed_array)
+    for seed in seed_array:
+        # print(seed)
+        current_map_value = seed
+        while (current_key in name_dictionary):
+            target = name_dictionary[current_key]
+            current_map_value = calculate_target_mapping(current_map_value, mapping_dictionary[target])
+            current_key = target
+        locations.append(current_map_value)
+        current_key = 'seed'
+    return min(locations)
+
 # solution functions
 def part_a(input):
-    # TODO
-    return
+    seed_array = construct_seed_array(input,'a')
+    return solution_impl(input, seed_array)    
 
 def part_b(input):
-    # TODO
-    return
+    seed_array = construct_seed_array(input, 'a')
+    # return solution_impl(seed_array)  
+    mapping_dictionary = {}
+    name_dictionary = {}
+    # index into dictionary based on source string
+    # determine mapping value
+    # find next dictionary based on target string
+    current_target = ''
+    print(input)
+    for index in range(2, len(input)):
+        s = input[index]
+        # print(s)
+        if s == '':
+            continue
+        parsed_entry = s.split(' ')[0]
+        if not parsed_entry.isdigit():
+            parsed_mapping_names = parsed_entry.split('-')
+            # print(parsed_mapping_names)
+            source = parsed_mapping_names[0]
+            target = parsed_mapping_names[2]
+            name_dictionary[source] = target
+            current_target = target
+            mapping_dictionary[target] = []
+        else:
+            parsed_mapping_values = lmap(lambda x: int(x), s.split(' '))
+            mapping_dictionary[current_target].append(parsed_mapping_values)
+    
+    current_key = 'seed'
+    
+    # TODO: this is a brute force approach and it needs to be optimized
+    lowest = None
+    for index in range(0, len(seed_array), 2):
+        for seed in range(seed_array[index], seed_array[index] + seed_array[index + 1], 1):
+            current_map_value = seed
+            while (current_key in name_dictionary):
+                target = name_dictionary[current_key]
+                current_map_value = calculate_target_mapping(current_map_value, mapping_dictionary[target])
+                current_key = target
+            # locations.append(current_map_value)
+            if lowest is None or current_map_value < lowest:
+                print(current_map_value)
+                lowest = current_map_value
+        
+            current_key = 'seed'
+    return lowest
 
-input_data = read_aoc_data(5, 2023)    # replace with correct day/year
+input_data = read_aoc_data(5, 2023)
 start_time = time.perf_counter()
 print(part_a(input_data))
 end_time = time.perf_counter()
--- 5/tests.py
+++ 5/tests.py
@@ -1,19 +1,39 @@
 import unittest
-from problem import *
+from problem import part_a, part_b
 
 class Tests(unittest.TestCase):
 
     def test_part_a(self):
-        expected_result = 'result'
-        sample_data = 'data'
+        expected_result = 35
+        sample_data = '''seeds: 79 14 55 13
+        
+seed-to-soil map:
+50 98 2
+52 50 48
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+water-to-light map:
+88 18 7
+18 25 70
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+temperature-to-humidity map:
+0 69 1
+1 0 69
+humidity-to-location map:
+60 56 37
+56 93 4'''.splitlines()
         result = part_a(sample_data)
-        self.assertEqual(result, expected_result)
-
-    def test_part_b(self):
-        expected_result = 'result'
-        sample_data = 'data'
-        result = part_b(sample_data)
-        self.assertEqual(result, expected_result)
+        self.assertEqual(result, expected_result)    
 
 if __name__ == '__main__':
     unittest.main()
