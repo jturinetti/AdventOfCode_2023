c88e6f2b40a0debd85470b06811b0df0c942a77c
= <jeremy.turinetti@gmail.com>
2023-12-05 17:10:17 -0600

    trying to optimize day 5

--- 5/problem.py
+++ 5/problem.py
@@ -17,7 +17,7 @@ def calculate_target_mapping(source_number, mapping_lists):
 def construct_seed_array(input):
     return lmap(lambda x: int(x), input[0].split(' ')[1::])
 
-def populate_mapping_dictionaries(input, mapping_dict, name_dict):
+def populate_mapping_dictionaries(input, mapping_dict, name_dict, reverse_name_dict):
     current_target = ''
     for index in range(2, len(input)):
         s = input[index]
@@ -29,6 +29,7 @@ def populate_mapping_dictionaries(input, mapping_dict, name_dict):
             source = parsed_mapping_names[0]
             target = parsed_mapping_names[2]
             name_dict[source] = target
+            reverse_name_dict[target] = source
             current_target = target
             mapping_dict[target] = []
         else:
@@ -38,8 +39,9 @@ def populate_mapping_dictionaries(input, mapping_dict, name_dict):
 def solution_impl(input, seed_array):
     mapping_dictionary = {}
     name_dictionary = {}
+    reverse_name_dictionary = {}
     
-    populate_mapping_dictionaries(input, mapping_dictionary, name_dictionary)
+    populate_mapping_dictionaries(input, mapping_dictionary, name_dictionary, reverse_name_dictionary)
 
     current_key = 'seed'
     lowest = None
@@ -61,19 +63,43 @@ def solution_impl(input, seed_array):
 # solution functions
 def part_a(input):
     seed_array = construct_seed_array(input)
-    return solution_impl(input, seed_array)    
+    return solution_impl(input, seed_array)
 
 def part_b(input):
     seed_array = construct_seed_array(input)
     mapping_dictionary = {}
+    reverse_name_dictionary = {}
     name_dictionary = {}
    
-    populate_mapping_dictionaries(input, mapping_dictionary, name_dictionary)
-
-    current_key = 'seed'
+    populate_mapping_dictionaries(input, mapping_dictionary, name_dictionary, reverse_name_dictionary)
     
+    # TODO: WIP - this currently wrong
+    current_key = 'location'
+    for potential_location in range(min(seed_array), max(flatten(mapping_dictionary[current_key])), 1):
+        current_map_value = potential_location
+        
+        while (current_key in reverse_name_dictionary):
+            target = reverse_name_dictionary[current_key]
+            
+            if target == 'seed':
+                for seed_index in range(0, len(seed_array), 2):
+                   
+                    if current_map_value >= seed_array[seed_index] and current_map_value <= (seed_array[seed_index] + seed_array[seed_index + 1]):
+                        return potential_location
+                    else:
+                        break
+            else:
+                current_map_value = calculate_target_mapping(current_map_value, mapping_dictionary[target])
+            
+            current_key = target
+        
+        current_key = 'location'
+    return -1
+
     # TODO: this is a brute force approach and it needs to be optimized
     lowest = None
+    current_key = 'seed'
+    
     for index in range(0, len(seed_array), 2):
         for seed in range(seed_array[index], seed_array[index] + seed_array[index + 1], 1):
             current_map_value = seed
@@ -101,7 +127,7 @@ def execute():
     print(f"part_b perf: {(end_time - start_time):02f}")
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, 
+    logging.basicConfig(level=logging.DEBUG, 
                         format='%(asctime)s %(funcName)s %(message)s', 
                         handlers=[logging.StreamHandler()])
     execute()
