bc6db96fe00251263b4dc76cc65d4407e6ff9d00
= <jeremy.turinetti@gmail.com>
2023-12-08 10:13:38 -0600

    day 8 solution and tests

--- 8/problem.py
+++ 8/problem.py
@@ -2,18 +2,71 @@ import time
 import logging
 from utils import *
 from my_utils import read_aoc_data
+from math import lcm
+
+def construct_dictionary(mapping_list):
+    dict = {}
+    for mapping in mapping_list:
+        key = mapping[:3]
+        val1 = mapping[7:10]
+        val2 = mapping[12:15]
+        dict[key] = (val1, val2)
+    return dict
+
+def navigate_to_next_node(dictionary, key, instruction_char):
+    if instruction_char == 'L':
+        return dictionary[key][0]
+    else:
+        return dictionary[key][1]
 
 # solution functions
 def part_a(input):
-    # TODO
-    return
+    instructions = input[0]
+    dictionary = construct_dictionary(input[2:])
+    step_counter = 0
+    instruction_index = 0
+    current = 'AAA'
+    while current != 'ZZZ':
+        step_counter += 1
+        next_instruction = instructions[instruction_index]
+        instruction_index += 1
+        if instruction_index == len(instructions):
+            instruction_index = 0
+        current = navigate_to_next_node(dictionary, current, next_instruction)
+    return step_counter
 
 def part_b(input):
-    # TODO
-    return
+    instructions = input[0]
+    dictionary = construct_dictionary(input[2:])
+    step_counter = 0
+    instruction_index = 0
+    instruction_tree_index = 0
+    current_instruction_list = [x for x in dictionary.keys() if str.endswith(x, 'A')]
+    next_instruction = instructions[instruction_index]
+    lcm_dict = {}
+    while not all(str.endswith(x, 'Z') for x in current_instruction_list):
+        current_instruction_list[instruction_tree_index] = navigate_to_next_node(dictionary, current_instruction_list[instruction_tree_index], next_instruction)
+        
+        if str.endswith(current_instruction_list[instruction_tree_index], 'Z') and instruction_tree_index not in lcm_dict:
+            lcm_dict[instruction_tree_index] = step_counter + 1
+    
+        if len(list(lcm_dict.keys())) == len(current_instruction_list):
+            return lcm(*[v for v in lcm_dict.values()])
+            
+        instruction_tree_index += 1
+        if instruction_tree_index == len(current_instruction_list):
+            step_counter += 1
+            instruction_index += 1
+            instruction_tree_index = 0
+            if instruction_index == len(instructions):
+                instruction_index = 0
+            next_instruction = instructions[instruction_index]
+
+    # we should never get to this point; lcm result should be returned first always        
+    return step_counter
 
 def execute():
-    input_data = read_aoc_data(1, 2023)    # replace with correct day and year
+    input_data = read_aoc_data(8, 2023)    # replace with correct day and year
     start_time = time.perf_counter()
     logging.info('part_a answer: {}'.format(part_a(input_data)))
     end_time = time.perf_counter()
--- 8/unit_test.py
+++ 8/unit_test.py
@@ -4,18 +4,49 @@ import logging
 from problem import part_a, part_b
 
 class Tests(unittest.TestCase):
-    test_data = ''  # ADD THIS FROM PROBLEM STATEMENT
+    part_a_test_data = '''RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)'''.splitlines()
+    
+    part_a_2_test_data = '''LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)'''.splitlines()
+
+    part_b_test_data = '''LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)'''.splitlines()
 
     @pytest.mark.timeout(10)
     def test_part_a(self):
-        expected_result = 'val'     # REPLACE WITH CORRECT SAMPLE VALUE (PART A)
-        result = part_a(self.test_data)
+        expected_result = 2
+        result = part_a(self.part_a_test_data)
+        self.assertEqual(result, expected_result)
+
+    @pytest.mark.timeout(10)
+    def test_part_a(self):
+        expected_result = 6
+        result = part_a(self.part_a_2_test_data)
         self.assertEqual(result, expected_result)
 
     @pytest.mark.timeout(10)
     def test_part_b(self):
-        expected_result = 'val'     # REPLACE WITH CORRECT SAMPLE VALUE (PART B)
-        result = part_b(self.test_data)
+        expected_result = 6
+        result = part_b(self.part_b_test_data)
         self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
