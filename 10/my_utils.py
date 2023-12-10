362289821c41bcc6fea3d9fbd3e5938539f1dec7
= <jeremy.turinetti@gmail.com>
2023-12-09 20:30:03 -0600

    day 10 prep

---
+++ 10/my_utils.py
@@ -0,0 +1,18 @@
+from aocd import get_data   # to retrieve puzzle inputs
+
+# file parsing utilities
+def read_input_file(filename):
+    input_file = open(filename, 'r')
+    return input_file.readlines()
+
+# math operations
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+# wrapper to call aocd api to retrieve input for day and year
+def read_aoc_data(day, year):
+    problem_data = get_data(day=day, year=year)
+    return problem_data.splitlines()
