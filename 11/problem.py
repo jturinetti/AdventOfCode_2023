e72415235ce50b961d10a2e482427a7fff9989c2
= <jeremy.turinetti@gmail.com>
2023-12-12 15:56:37 -0600

    day 11 solution post-cleanup

--- 11/problem.py
+++ 11/problem.py
@@ -3,17 +3,62 @@ import logging
 from utils import *
 from my_utils import read_aoc_data
 
+def parse_input(input):
+    galaxy_coords = []
+    empty_rows = []
+    empty_cols = []
+    for row_index in range(len(input)):
+        found = False
+        for col_index in range(len(input[row_index])):
+            if input[row_index][col_index] == '#':
+                galaxy_coords.append((row_index, col_index))
+                found = True
+        if not found:
+            empty_rows.append(row_index)
+    
+    # find empty columns
+    for col_index in range(len(input[0])):
+        found = False
+        for row in input:
+            if row[col_index] == '#':
+                found = True
+
+        if not found: 
+            empty_cols.append(col_index)
+    
+    return (galaxy_coords, empty_rows, empty_cols)
+
+def get_distance(coord_1, coord_2, empty_rows, empty_cols, expansion_factor):
+    # return math.sqrt((coord_2[0] - coord_1[0])**2 + (coord_2[1] - coord_1[1])**2)
+    # use Manhattan distance for this
+    raw_sum = abs((coord_2[0] - coord_1[0])) + abs(coord_2[1] - coord_1[1])
+    expanded_rows = (expansion_factor - 1) * len(list(filter(lambda r: (coord_1[0] > r > coord_2[0]) or (coord_2[0] > r > coord_1[0]), empty_rows)))
+    expanded_cols = (expansion_factor - 1) * len(list(filter(lambda c: (coord_1[1] > c > coord_2[1]) or (coord_2[1] > c > coord_1[1]), empty_cols)))
+    
+    return raw_sum + expanded_cols + expanded_rows
+
+def solution_impl(galaxy_coords, empty_rows, empty_cols, expansion_factor):
+    sum = 0
+    cur_galaxy = 0
+    while cur_galaxy < len(galaxy_coords) - 1:
+        for other_galaxy in range(cur_galaxy + 1, len(galaxy_coords)):
+            val = get_distance(galaxy_coords[cur_galaxy], galaxy_coords[other_galaxy], empty_rows, empty_cols, expansion_factor)
+            logging.debug('{} and {}: {}'.format(galaxy_coords[cur_galaxy], galaxy_coords[other_galaxy], val))
+            sum += val
+        cur_galaxy += 1
+    return sum
+
 # solution functions
 def part_a(input):
-    # TODO
-    return
+    (galaxy_coords, empty_rows, empty_cols) = parse_input(input)
+    return solution_impl(galaxy_coords, empty_rows, empty_cols, 2)
 
-def part_b(input):
-    # TODO
-    return
+def part_b(input, expansion_factor = 1000000):
+    (galaxy_coords, empty_rows, empty_cols) = parse_input(input)
+    return solution_impl(galaxy_coords, empty_rows, empty_cols, expansion_factor)
 
 def execute():
-    input_data = read_aoc_data(11, 2023)    # replace with correct day and year
+    input_data = read_aoc_data(11, 2023)
     start_time = time.perf_counter()
     logging.info('part_a answer: {}'.format(part_a(input_data)))
     end_time = time.perf_counter()
--- 11/unit_test.py
+++ 11/unit_test.py
@@ -5,18 +5,34 @@ from my_utils import *
 from problem import part_a, part_b
 
 class Tests(unittest.TestCase):
-    test_data = parse_multi_line_input('')  # ADD THIS FROM PROBLEM STATEMENT
+    test_data = parse_multi_line_input('''
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....''')
 
     @pytest.mark.timeout(10)
     def test_part_a(self):
-        expected_result = 'val'     # REPLACE WITH CORRECT SAMPLE VALUE (PART A)
+        expected_result = 374
         result = part_a(self.test_data)
         self.assertEqual(result, expected_result)
 
     @pytest.mark.timeout(10)
-    def test_part_b(self):
-        expected_result = 'val'     # REPLACE WITH CORRECT SAMPLE VALUE (PART B)
-        result = part_b(self.test_data)
+    def test_part_b_1(self):
+        expected_result = 1030
+        result = part_b(self.test_data, 10)
+        self.assertEqual(result, expected_result)
+    
+    @pytest.mark.timeout(10)
+    def test_part_b_2(self):
+        expected_result = 8410
+        result = part_b(self.test_data, 100)
         self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
