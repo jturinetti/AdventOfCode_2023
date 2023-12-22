21dbe1fbfb9354c7c4ba0d5d34555c03cb8c2b96
= <jeremy.turinetti@gmail.com>
2023-12-21 22:54:38 -0600

    day 21 and 22 prep

---
+++ 22/problem.py
@@ -0,0 +1,30 @@
+import time
+import logging
+from utils import *
+from my_utils import read_aoc_data
+
+# solution functions
+def part_a(input):
+    # TODO
+    return
+
+def part_b(input):
+    # TODO
+    return
+
+def execute():
+    input_data = read_aoc_data(22, 2023)    # replace with correct day and year
+    start_time = time.perf_counter()
+    logging.info('part_a answer: {}'.format(part_a(input_data)))
+    end_time = time.perf_counter()
+    logging.info(f"part_a perf: {(end_time - start_time):02f}s")
+    start_time = time.perf_counter()
+    logging.info('part_b answer: {}'.format(part_b(input_data)))
+    end_time = time.perf_counter()
+    logging.info(f"part_b perf: {(end_time - start_time):02f}s")
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, 
+                        format='[%(asctime)s][%(levelname)-5s] : %(message)s', 
+                        handlers=[logging.StreamHandler()])
+    execute()
