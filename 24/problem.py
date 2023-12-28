119c8def5b4fca460f2cc5b7b255b61e95d779fa
= <jeremy.turinetti@gmail.com>
2023-12-28 15:29:56 -0600

    day 24 part 1

--- 24/problem.py
+++ 24/problem.py
@@ -12,22 +12,61 @@ def parse(row):
     return (coord, slope)
 
 # solution functions
-def part_a(input):
-    bounds_min = 200000000000000
-    bounds_max = 400000000000000
+def part_a(input, bounds_min = 200000000000000, bounds_max = 400000000000000):
+    counter = 0
     for p1_index in range(len(input)):
+        logging.debug('OUTER LOOP INCREMENT ({} of {})'.format(p1_index + 1, len(input)))
         for p2_index in range(p1_index + 1, len(input)):
-            (p1_coord, p1_slope) = parse(input[p1_index])
-            (p2_coord, p2_slope) = parse(input[p2_index])
+            (p1_coord, p1_slope_data) = parse(input[p1_index])
+            (p2_coord, p2_slope_data) = parse(input[p2_index])
+            p1_slope = p1_slope_data[1] / p1_slope_data[0]
+            p2_slope = p2_slope_data[1] / p2_slope_data[0]
+
+            # if equal, they are parallel or they coincide
+            if p1_slope == p2_slope:
+                logging.debug('  SLOPES EQUAL; SKIPPING')
+                continue
+
+            logging.debug('p1 coord: {}'.format(p1_coord))
+            logging.debug('p1 slope: {}'.format(p1_slope))
+            logging.debug('p2 coord: {}'.format(p2_coord))
+            logging.debug('p2 slope: {}'.format(p2_slope))
+
+            l1_y_int = p1_coord[1] - (p1_slope * p1_coord[0])
+            l2_y_int = p2_coord[1] - (p2_slope * p2_coord[0])
+            int_x = (l2_y_int - l1_y_int) / (p1_slope - p2_slope)
+            int_y = (int_x * p1_slope) + l1_y_int
             
-    return
+            logging.debug('  int_x: {}'.format(int_x))
+            logging.debug('  int_y: {}'.format(int_y))
+            
+            if (int_x >= bounds_min and int_y >= bounds_min and int_x <= bounds_max and int_y <= bounds_max):
+                if (p1_slope_data[0] < 0 and int_x > p1_coord[0]) or (p1_slope_data[0] > 0 and int_x < p1_coord[0]):
+                    logging.debug('PAST X P1')
+                    continue
+                if (p1_slope_data[1] < 0 and int_y > p1_coord[1]) or (p1_slope_data[1] > 0 and int_y < p1_coord[1]):
+                    logging.debug('PAST Y P1')
+                    continue
+                if (p2_slope_data[0] < 0 and int_x > p2_coord[0]) or (p2_slope_data[0] > 0 and int_x < p2_coord[0]):
+                    logging.debug('PAST X P2')
+                    continue
+                if (p2_slope_data[1] < 0 and int_y > p2_coord[1]) or (p2_slope_data[1] > 0 and int_y < p2_coord[1]):
+                    logging.debug('PAST Y P2')
+                    continue
+                    
+                logging.debug('   WITHIN BOUNDS!')
+                counter += 1
+            else:
+                logging.debug('   NOT WITHIN BOUNDS.')
+            
+    return counter
 
 def part_b(input):
     # TODO
     return
 
 def execute():
-    input_data = read_aoc_data(24, 2023)    # replace with correct day and year
+    input_data = read_aoc_data(24, 2023)
     start_time = time.perf_counter()
     logging.info('part_a answer: {}'.format(part_a(input_data)))
     end_time = time.perf_counter()
--- 24/unit_test.py
+++ 24/unit_test.py
@@ -5,19 +5,23 @@ from my_utils import *
 from problem import part_a, part_b
 
 class Tests(unittest.TestCase):
-    test_data = parse_multi_line_input('')  # ADD THIS FROM PROBLEM STATEMENT
+    test_data = parse_multi_line_input('''19, 13, 30 @ -2,  1, -2
+18, 19, 22 @ -1, -1, -2
+20, 25, 34 @ -2, -2, -4
+12, 31, 28 @ -1, -2, -1
+20, 19, 15 @  1, -5, -3''')
 
     @pytest.mark.timeout(10)
     def test_part_a(self):
-        expected_result = 'val'     # REPLACE WITH CORRECT SAMPLE VALUE (PART A)
-        result = part_a(self.test_data)
+        expected_result = 2
+        result = part_a(self.test_data, 7, 27)
         self.assertEqual(result, expected_result)
 
-    @pytest.mark.timeout(10)
-    def test_part_b(self):
-        expected_result = 'val'     # REPLACE WITH CORRECT SAMPLE VALUE (PART B)
-        result = part_b(self.test_data)
-        self.assertEqual(result, expected_result)
+    # @pytest.mark.timeout(10)
+    # def test_part_b(self):
+    #     expected_result = 'val'     # REPLACE WITH CORRECT SAMPLE VALUE (PART B)
+    #     result = part_b(self.test_data)
+    #     self.assertEqual(result, expected_result)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, 
